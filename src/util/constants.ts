5e63fd0591e885f972ad6263073c0714a702292e
doinkythederp <mcclelll@icloud.com>
2021-12-18 19:37:46 -0800

    feat: add token constants

---
+++ src/util/constants.ts
@@ -0,0 +1,11 @@
+// tokens
+export const ESCAPE = '\\';
+export const TEMPLATE_STRING = '`';
+export const SINGLE_QUOTE = "'";
+export const DOUBLE_QUOTE = '"';
+export const TAG_START = '{';
+export const TAG_END = '}';
+export const COMMENT_MARKER = '!';
+export const SEPERATOR = ' ';
+export const ASSIGNMENT_OPERATOR = '=';
+export const VARIABLE_ACCESSOR = '$';
