a3569bac7c8551bbbd5d88b83c1fbf9207cbe42d
doinkythederp <mcclelll@icloud.com>
2022-01-01 13:59:11 -0800

    feat: export values from entry point

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
-export {};
+export * from './Node';
+export * from './util/errors';
+export * from './interpreters';
---
+++ src/interpreters/index.ts
@@ -0,0 +1,17 @@
+export * from './FunctionCallInterpreter';
+export { default as FunctionCallInterpreter } from './FunctionCallInterpreter';
+
+export * from './NumberInterpreter';
+export { default as NumberInterpreter } from './NumberInterpreter';
+
+export * from './PropertyAccessorInterpreter';
+export { default as PropertyAccessorInterpreter } from './PropertyAccessorInterpreter';
+
+export * from './StringInterpreter';
+export { default as StringInterpreter } from './StringInterpreter';
+
+export * from './ValueInterpreter';
+export { default as ValueInterpreter } from './ValueInterpreter';
+
+export * from './VariableAccessorInterpreter';
+export { default as VariableAccessorInterpreter } from './VariableAccessorInterpreter';
