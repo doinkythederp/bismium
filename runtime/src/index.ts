ab168479aa1a3c613d6149c945fa62b565b21289
doinkythederp <mcclelll@icloud.com>
2022-01-15 13:17:40 -0800

    fix(Runtime): export values from entry point

---
+++ runtime/src/index.ts
@@ -0,0 +1,18 @@
+export * from './Runtime';
+export { default as Runtime } from './Runtime';
+
+export * from './values';
+export * from './values/BismiumObject';
+export { default as BismiumObject } from './values/BismiumObject';
+export * from './values/BismiumFunction';
+export { default as BismiumFunction } from './values/BismiumFunction';
+export * from './values/primitives/BismiumBoolean';
+export { default as BismiumBoolean } from './values/primitives/BismiumBoolean';
+export * from './values/primitives/BismiumNull';
+export { default as BismiumNull } from './values/primitives/BismiumNull';
+export * from './values/primitives/BismiumNumber';
+export { default as BismiumNumber } from './values/primitives/BismiumNumber';
+export * from './values/primitives/BismiumString';
+export { default as BismiumString } from './values/primitives/BismiumString';
+
+export * from './util/errors';
